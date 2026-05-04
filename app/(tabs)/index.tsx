552a5a5fa7c17f17d0d10648d3d31b1ad28b9b93
Devraj Chouhan <chouhanaman525@gmail.com>
2026-05-04 18:40:58 +0530

    feat: project setup, nativewind, routing , tab navigation

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,14 @@
 import "@/global.css"
 import {Text, View} from "react-native";
 import {Link} from "expo-router";
+import {SafeAreaView, SafeAreaView as RNSafeAreaView} from "react-native-safe-area-context";
+import {styled} from "nativewind";
+const SafeAreaView = styled(RNSafeAreaView);
 
 export default function App() {
+    // @ts-ignore
     return (
-        <View className="flex-1 items-center justify-center bg-background">
+        <SafeAreaView >
             <Text className="text-xl font-bold text-success">
                 Welcome to Nativewind!
             </Text>
@@ -14,10 +18,10 @@ export default function App() {
                 sign-in </Link>
             <Link href="/(auth)/sign-up" className="mt-4 rounded bg-primary text-white p-4">Click Here for
                 sign-up </Link>
-            <Link href="/subscriptions/spotify" className="mt-4 rounded bg-primary text-white p-4">spotify
+            <Link href="/my-app/app/subscriptions/spotify" className="mt-4 rounded bg-primary text-white p-4">spotify
                 subscription </Link>
             <Link href={{pathname:"/subscriptions/[id]",params:{id:"claude"},}}>claude subscription</Link>
-        </View>
+        </SafeAreaView>
     );
 
 
--- app/subscriptions/[id].tsx
+++ app/subscriptions/[id].tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import {Link, useLocalSearchParams} from "expo-router";
+import { SafeAreaView as RNSafeAreaView} from "react-native-safe-area-context";
+import {styled} from "nativewind";
+const SafeAreaView = styled(RNSafeAreaView);
 
 const SubscriptDetails = () => {
     const {id} = useLocalSearchParams<{id:string}>();
   return (
-    <View>
+    <SafeAreaView className={"flex-1 bg-background p-5"}>
       <Text>SubscriptDetails : {id}</Text>
         <Link href="/">GO Back</Link>
-    </View>
+    </SafeAreaView>
   );
 };
 
