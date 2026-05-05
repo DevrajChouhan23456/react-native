9f9afafa3171c98607a17dd10dfcf5cd030014c2
Devraj Chouhan <chouhanaman525@gmail.com>
2026-05-05 13:52:16 +0530

    impliment fonts

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,26 +1,23 @@
 import "@/global.css"
 import {Text} from "react-native";
 import {Link} from "expo-router";
-import {SafeAreaView, SafeAreaView as RNSafeAreaView} from "react-native-safe-area-context";
+import { SafeAreaView as RNSafeAreaView} from "react-native-safe-area-context";
 import {styled} from "nativewind";
 const SafeAreaView = styled(RNSafeAreaView);
 
 export default function App() {
     // @ts-ignore
     return (
-        <SafeAreaView >
-            <Text className="text-xl font-bold text-success">
-                Welcome to Nativewind!
+        <SafeAreaView className="flex-1 bg-background p-5">
+            <Text className="text-5xl font-sans-extrabold text-primary">
+                Home
             </Text>
-            <Link href="/onboarding" className="mt-4 rounded bg-primary text-white p-4">Click Here for
+            <Link href="/onboarding" className="mt-4 font-sans-bold  rounded bg-primary text-white p-4">Click Here for
                 onboarding </Link>
-            <Link href="/(auth)/sign-in" className="mt-4 rounded bg-primary text-white p-4">Click Here for
+            <Link href="/(auth)/sign-in" className="mt-4  font-sans-boldrounded bg-primary text-white p-4">Click Here for
                 sign-in </Link>
-            <Link href="/(auth)/sign-up" className="mt-4 rounded bg-primary text-white p-4">Click Here for
+            <Link href="/(auth)/sign-up" className="mt-4 font-sans-bold rounded bg-primary text-white p-4">Click Here for
                 sign-up </Link>
-            <Link href="/subscriptions/spotify" className="mt-4 rounded bg-primary text-white p-4">spotify
-                subscription </Link>
-            <Link href={{pathname:"/subscriptions/[id]",params:{id:"claude"},}}>claude subscription</Link>
         </SafeAreaView>
     );
 
